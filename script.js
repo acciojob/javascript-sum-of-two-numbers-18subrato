f9bfbe89d5318c8381a1d041720ce7a68b01fcec
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-26 15:26:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let input1 = prompt("Enter the first number:");
+let input1 = prompt("Enter the first number:"
 let input2 = prompt("Enter the second number:");
 
-// Write your code here and print the output using alert function
+let input1 = Number(input1);
+let input2 = Number(input2);
+if(typeof input1 === 'NAN'){
+	alert('Invalid input. Please enter a valid number.')
+} else if(typeof input2 === 'NAN'){
+	alert('Invalid input. Please enter a valid number.')
+} else {
+	alert(`The sum of ${input1} and ${input2} is ${input1+input2}`)
+}
